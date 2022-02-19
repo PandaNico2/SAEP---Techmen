436da552eee41745fea8668bed0aa1bb8f425ab9
Isadora M <91219205+PandaNico2@users.noreply.github.com>
2022-02-18 22:11:38 -0300

    CSS

--- listar.php
+++ listar.php
@@ -1,30 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
 
-    
+
     <title>Produto</title>
 </head>
-<body>
 
-<?php
+<body id="listar.php">
 
-require './classes/Produto.php';
-$produto = new Produto();
-$listaProduto = $produto->listar();
+    <div id="header">
+
+        <img src="img/techman.png" alt="" id="logoHeder">
+
+        <a href="adicionar.php">Novo equipamento</a>
+
+        <a href="index.php">
+            <img src="img/logout_sair.png" alt="" id="logout">
+        </a>
+
+    </div>
+
+    <?php
+
+    require './classes/Produto.php';
+    $produto = new Produto();
+    $listaProduto = $produto->listar();
 
     ?>
 
     <center>
         <br>
 
-    
-    <a href="adicionar.php">
-        <button 
-    type="button" class="btn btn-warning">Adicionar</button>
-    <br></a><br>
+
+
 
 
         <table class="table table-dark table-striped table-hover" style="width: 70%; margin-top:30px;  border: 1px solid white;">
@@ -33,7 +45,7 @@ $listaProduto = $produto->listar();
                     <td>Imagen</td>
                     <td>Produto</td>
                     <td>Descrição</td>
-                    
+
                 </tr>
             </thead>
 
@@ -41,21 +53,21 @@ $listaProduto = $produto->listar();
                 <?php foreach ($listaProduto as $c) { ?>
                     <tr>
 
-                    <td></td>
+                        <td></td>
                         <td><i class="fas fa-address-card"></i> <?php echo $c['id']; ?></td>
                         <td><i class="fas fa-address-card"></i> <?php echo $c['nome']; ?></td>
                         <td><i class="fas fa-address-card"></i> <?php echo $c['descricao']; ?></td>
                         <td>
-                            <a href="excluir.php?id=<?php echo $c ['id']; ?>">
-                            <button type="button" class="btn btn-danger">Apagar</button>
+                            <a href="excluir.php?id=<?php echo $c['id']; ?>">
+                                <img src="img/deletar.png" alt="" width="30px">
                             </a>
                         </td>
 
-                        <td><i class="far fa-user"></i> 
-                            <a href="editar.php?id=<?php echo $c ['id']; ?>">
-                            <button type="button" class="btn btn-info">Editar</button>
+                        <td><i class="far fa-user"></i>
+                            <a href="editar.php?id=<?php echo $c['id']; ?>">
+                                <img src="img/editar.png" alt="" width="25px">
                             </a>
-                            
+
                         </td>
 
                     </tr>
@@ -68,9 +80,12 @@ $listaProduto = $produto->listar();
 
     <br><br><br>
 
-    <a href="comit_listar.php">comentario</a>
+    <a href="comit_listar.php">
+        <img src="img/comentario.png" alt="" width="30px">
+    </a>
 
 
 
 </body>
+
 </html>
